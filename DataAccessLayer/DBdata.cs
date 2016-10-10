6413ff61316ecf4a82d9316b96e4d3cb6e3c1fbc
iTravelApps-2016 <sid@itravelapps.co>
2016-10-10 13:26:47 +0530

    Add files via upload

---
+++ DataAccessLayer/DBdata.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using FourQuarterMVC.Models;
+
+namespace FourQuarterMVC.DataAccessLayer
+{
+    public class DBdata
+    {
+        #region Variables Used
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
+        SqlDataAdapter da;
+        SqlCommand cmd;
+        DataTable dt;
+
+        #endregion
+
+        #region Insert Action..........
+
+        public string insertTitle(UpdateTitle UT)
+        {
+            string result = "";
+            try
+            {
+            cmd = new SqlCommand("SpHomeModule",con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@DDLValue",UT.DDlValues);
+            cmd.Parameters.AddWithValue("@Title1",UT.Title);
+            cmd.Parameters.AddWithValue("@Title2",UT.SubTitle);
+            cmd.Parameters.AddWithValue("@ImagePath",UT.ImagePath);
+            cmd.Parameters.AddWithValue("@Div1",UT.Div1);
+            cmd.Parameters.AddWithValue("@Div1",UT.Div1);
+            cmd.Parameters.AddWithValue("@Div1",UT.Div1);
+            cmd.Parameters.AddWithValue("@Qtype","Insert");
+            con.Open();
+            result = cmd.ExecuteScalar().ToString();
+            return result;
+
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        
+        #endregion
+    }
+}
